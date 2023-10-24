0cb5afbcad710ac0f49b1bddf80705e1a369ad32
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-10-24 14:19:28 +0300

    add Component App and add router and import another Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+//Components
+import MainPage from "./pages/MainPage";
+import MainMenu from "./pages/GameOverMenu";
+import Game from "./pages/Game";
+//style
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="*" element={<MainPage />} />
+        <Route path="/startGame" element={<Game />} />
+        <Route path="/menu" element={<MainMenu />} />
+      </Routes>
     </div>
   );
 }
